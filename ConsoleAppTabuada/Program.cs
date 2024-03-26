8a15bcfed4a4e5818cbc0c15728f227401c49b53
Usuario <Usuario@CPD02-M01>
2024-03-26 11:19:58 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleAppTabuada/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppTabuada
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a Tabuada que deseja saber: ");
+            int tabuada = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o numero de vezes: ");
+            int vezes = int.Parse(Console.ReadLine());
+            int cont = 1;
+
+            while (cont <= vezes) 
+            {
+                int res = tabuada * cont;
+                Console.WriteLine(tabuada+ "x" +cont+ "=" +res);
+                cont++;
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
